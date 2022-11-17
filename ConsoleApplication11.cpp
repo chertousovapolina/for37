858d0546702b9f70fe57a39ff77b77e074787358
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-17 14:22:53 +0300

    Add files via upload

---
+++ ConsoleApplication11.cpp
@@ -0,0 +1,18 @@
+﻿// дз чертоусова for37
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int k, n, sum = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		sum += pow(i, i);
+	}
+	cout << sum << " ";
+
+
+
+
+}
+
